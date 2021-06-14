f87be50567fca18d11c41604bfd8467b9dcbef5a
Arsen Tadevosyan <46923880+Hazard45@users.noreply.github.com>
2021-06-14 15:08:39 +0400

    commit 2

--- ProducerApp/Program.cs
+++ ProducerApp/Program.cs
@@ -7,6 +7,22 @@ namespace ProducerApp
 {
     class Program
     {
+        /*
+         * Databse first approach.
+         * Create a database.
+         * Create a table.
+         * CREATE TABLE [dbo].[Tasks] (
+            [Id]               UNIQUEIDENTIFIER NOT NULL,
+            [TaskText]         NVARCHAR (50)    NULL,
+            [Status]           INT              NULL,
+            [CreationTime]     DATETIME2 (7)    NOT NULL,
+            [ModificationTime] DATETIME2 (7)    NOT NULL,
+            [ConsumerId]       UNIQUEIDENTIFIER NULL,
+            PRIMARY KEY CLUSTERED ([Id] ASC)
+          );
+         
+            Set connection string in TestDbContext.cs file
+         */
         static void Main(string[] args)
         {
             var dbContext = new TestDbContext();
